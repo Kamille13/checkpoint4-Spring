849c208bfc48625ade8ff05a9ca8a1639f155891
Camille Brunet <camille.brunet.07@hotmail.fr>
2019-07-16 10:45:39 +0200

    Configuration

---
+++ src/test/java/com/checkpoint4/checkpoint4/Checkpoint4ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.checkpoint4.checkpoint4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Checkpoint4ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
